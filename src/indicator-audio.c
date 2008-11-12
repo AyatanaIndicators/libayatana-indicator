b4b7ab38fd5990567da2713638e8edcd055e3eec
Ted Gould <ted@canonical.com>
2008-11-12 17:06:56 -0600

    Okay, audio is an icon now.

--- src/indicator-audio.c
+++ src/indicator-audio.c
@@ -5,7 +5,14 @@
 GtkWidget *
 indicator_audio_menuitem (void)
 {
+	GtkWidget * mainmenu = gtk_menu_item_new();
 
-	return NULL;
+	GtkWidget * icon = gtk_image_new_from_icon_name("audio-volume-muted",
+	                                                GTK_ICON_SIZE_MENU);
+
+	gtk_container_add(GTK_CONTAINER(mainmenu), icon);
+	gtk_widget_show(icon);
+
+	return mainmenu;
 }
 
