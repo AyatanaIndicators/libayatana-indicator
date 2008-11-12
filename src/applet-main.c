9d5abe0b9023f29fd91e43131bc473c5af926761
Ted Gould <ted@canonical.com>
2008-11-12 16:06:07 -0600

    Putting a label in the widget

--- src/applet-main.c
+++ src/applet-main.c
@@ -21,6 +21,14 @@ PANEL_APPLET_BONOBO_FACTORY ("OAFIID:GNOME_IndicatorApplet_Factory",
 static gboolean
 applet_fill_cb (PanelApplet * applet, const gchar * iid, gpointer data)
 {
+	GtkWidget * hbox = gtk_hbox_new(FALSE, 3);
+
+	GtkWidget * label = gtk_label_new("Test");
+	gtk_box_pack_end(GTK_BOX(hbox), label, TRUE, TRUE, 0);
+	gtk_widget_show(label);
+
+	gtk_container_add(GTK_CONTAINER(applet), hbox);
+	gtk_widget_show(hbox);
 
 	return TRUE;
 }
