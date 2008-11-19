04153d7d65573ee2dd0a19ca51aeb3b3e6b99a8d
Ted Gould <ted@canonical.com>
2008-11-19 14:25:56 -0600

    Adding in a menu item to start putting in a slider.

--- src/indicator-audio.c
+++ src/indicator-audio.c
@@ -3,6 +3,24 @@
 #include "indicator-audio.h"
 
 GtkWidget *
+create_output_menu_item (void)
+{
+	GtkWidget * menuitem = gtk_menu_item_new();
+
+	GtkWidget * label_hbox = gtk_hbox_new(FALSE, 12);
+
+	GtkWidget * label = gtk_label_new("Volume:");
+	gtk_box_pack_end(GTK_BOX(label_hbox), label, TRUE, FALSE, 0);
+	gtk_widget_show(label);
+
+
+	gtk_container_add(GTK_CONTAINER(menuitem), label_hbox);
+	gtk_widget_show(label_hbox);
+
+	return menuitem;
+}
+
+GtkWidget *
 indicator_audio_menuitem (void)
 {
 	GtkWidget * mainmenu = gtk_menu_item_new();
@@ -12,6 +30,15 @@ indicator_audio_menuitem (void)
 
 	gtk_container_add(GTK_CONTAINER(mainmenu), icon);
 	gtk_widget_show(icon);
+
+	GtkWidget * menu = gtk_menu_new();
+
+	GtkWidget * item = create_output_menu_item();
+	gtk_menu_append(GTK_MENU(menu), item);
+	gtk_widget_show(item);
+
+	gtk_menu_item_set_submenu(GTK_MENU_ITEM(mainmenu), menu);
+	gtk_widget_show(menu);
 
 	return mainmenu;
 }
