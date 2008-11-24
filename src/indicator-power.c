c6c57ca7532915d9de6cfee8cab437d379387b52
Ted Gould <ted@canonical.com>
2008-11-24 16:50:46 -0600

    Basic power handler

--- src/indicator-power.c
+++ src/indicator-power.c
@@ -5,7 +5,22 @@
 GtkWidget *
 indicator_power_menuitem (void)
 {
+	GtkWidget * mainmenu = gtk_menu_item_new();
 
-	return NULL;
+	GtkWidget * hbox = gtk_hbox_new(FALSE, 0);
+
+	GtkWidget * icon = gtk_image_new_from_icon_name("audio-volume-high",
+	                                                GTK_ICON_SIZE_MENU);
+	gtk_box_pack_start(GTK_BOX(hbox), icon, FALSE, FALSE, 0);
+	gtk_widget_show(icon);
+
+	GtkWidget * label = gtk_label_new("2:30");
+	gtk_box_pack_start(GTK_BOX(hbox), label, FALSE, FALSE, 0);
+	gtk_widget_show(label);
+
+	gtk_container_add(GTK_CONTAINER(mainmenu), hbox);
+	gtk_widget_show(hbox);
+
+	return mainmenu;
 }
 
