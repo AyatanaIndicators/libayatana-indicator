c8ae9fe01a30b09f2803edb85966303bd9850cad
Ted Gould <ted@canonical.com>
2008-11-24 22:49:50 -0600

    Adding in some filler menus to get some depth.

--- src/indicator-audio.c
+++ src/indicator-audio.c
@@ -3,22 +3,6 @@
 #include "slidermenuitem.h"
 #include "indicator-audio.h"
 
-void
-block_prelight (GtkWidget * widget, GtkStateType prev, gpointer data)
-{
-	if (GTK_WIDGET_STATE(widget) == GTK_STATE_PRELIGHT) {
-		gtk_widget_set_state(widget, prev);
-	}
-	return;
-}
-
-GtkWidget *
-create_output_menu_item (void)
-{
-	GtkWidget * menuitem = slider_menu_item_new("Volume:", 0.0, 1.0, 0.02);
-
-	return menuitem;
-}
 
 GtkWidget *
 indicator_audio_menuitem (void)
@@ -33,7 +17,23 @@ indicator_audio_menuitem (void)
 
 	GtkWidget * menu = gtk_menu_new();
 
-	GtkWidget * item = create_output_menu_item();
+	GtkWidget * item = gtk_menu_item_new_with_label("Mute All");
+	gtk_menu_append(GTK_MENU(menu), item);
+	gtk_widget_show(item);
+
+	item = gtk_menu_item_new_with_label("Sound Settings...");
+	gtk_menu_append(GTK_MENU(menu), item);
+	gtk_widget_show(item);
+
+	GtkWidget * sep = gtk_separator_menu_item_new();
+	gtk_menu_append(GTK_MENU(menu), sep);
+	gtk_widget_show(sep);
+
+	item = slider_menu_item_new("Speakers:", 0.0, 1.0, 0.02);
+	gtk_menu_append(GTK_MENU(menu), item);
+	gtk_widget_show(item);
+
+	item = slider_menu_item_new("Headphones:", 0.0, 1.0, 0.02);
 	gtk_menu_append(GTK_MENU(menu), item);
 	gtk_widget_show(item);
 
