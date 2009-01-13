bba89edeba9b17832b1f6971c603541ee5877c8c
Ted Gould <ted@canonical.com>
2009-01-13 13:38:29 -0600

    Return to crashing beauty

--- libindicate/tests/indicate-and-crash.c
+++ libindicate/tests/indicate-and-crash.c
@@ -12,7 +12,7 @@ main (int argc, char ** argv)
 	IndicateIndicator * indicator = indicate_indicator_new();
 	indicate_indicator_show(indicator);
 
-	//g_timeout_add_seconds(15, crashfunc, NULL);
+	g_timeout_add_seconds(15, crashfunc, NULL);
 
 	g_main_loop_run(g_main_loop_new(NULL, FALSE));
 
