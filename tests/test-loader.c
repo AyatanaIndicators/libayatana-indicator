1386478a87fd0f228dd31a00d8f3d969d6a6a98c
Ted Gould <ted@canonical.com>
2009-10-08 13:00:00 -0400

    Fleshing out the test a little bit.

--- tests/test-loader.c
+++ tests/test-loader.c
@@ -1,7 +1,29 @@
+#include <glib.h>
+#include "libindicator/indicator-object.h"
+
+void
+test_loader_refunref (void)
+{
+
+	return;
+}
+
+void
+test_loader_creation_deletion_suite (void)
+{
+	g_test_add_func ("/libindicator/loader/ref_and_unref", test_loader_refunref);
+
+	return;
+}
+
 
 int
 main (int argc, char ** argv)
 {
+	g_type_init (); 
+	g_test_init (&argc, &argv, NULL);
 
-	return 0;
+	test_loader_creation_deletion_suite();
+
+	return g_test_run();
 }
