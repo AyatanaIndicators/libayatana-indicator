755adf0afbb0b0478f6c9f3534f04f21bcdea910
Ted Gould <ted@canonical.com>
2009-10-28 16:41:34 -0500

    Oh, legal headers.  Fun, fun.

--- libindicator/indicator-object.c
+++ libindicator/indicator-object.c
@@ -1,3 +1,24 @@
+/*
+An interface for indicators to link to for creation.
+
+Copyright 2009 Canonical Ltd.
+
+Authors:
+    Ted Gould <ted@canonical.com>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 3.0 as published by the Free Software Foundation.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License version 3.0 for more details.
+
+You should have received a copy of the GNU General Public
+License along with this library. If not, see
+<http://www.gnu.org/licenses/>.
+*/
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
