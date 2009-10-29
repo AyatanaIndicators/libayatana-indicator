0af9206bc55a78a2e39ba86b07450a15ed399eee
Ted Gould <ted@canonical.com>
2009-10-29 09:56:39 -0500

    Defining some API and boot strapping documentation.

--- libindicator/indicator-service-manager.h
+++ libindicator/indicator-service-manager.h
@@ -13,19 +13,50 @@ G_BEGIN_DECLS
 #define INDICATOR_IS_SERVICE_MANAGER_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), INDICATOR_SERVICE_MANAGER_TYPE))
 #define INDICATOR_SERVICE_MANAGER_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), INDICATOR_SERVICE_MANAGER_TYPE, IndicatorServiceManagerClass))
 
+#define INDICATOR_SERVICE_MANAGER_SIGNAL_CONNECTION_CHANGE   "connection-change"
+
 typedef struct _IndicatorServiceManager      IndicatorServiceManager;
 typedef struct _IndicatorServiceManagerClass IndicatorServiceManagerClass;
 
+/**
+	IndicatorServiceManagerClass:
+	@parent: #GObjectClass
+	@connection_changed: Slot for #IndicatorServiceManager::connection-changed.
+	@indicator_service_manager_reserved1: Reserved for future use.
+	@indicator_service_manager_reserved2: Reserved for future use.
+	@indicator_service_manager_reserved3: Reserved for future use.
+	@indicator_service_manager_reserved4: Reserved for future use.
+
+*/
 struct _IndicatorServiceManagerClass {
 	GObjectClass parent_class;
 
+	/* Signals */
+	void (*connection_change) (IndicatorServiceManager * sm, gboolean connected, gpointer user_data);
+
+	/* Buffer */
+	void (*indicator_service_manager_reserved1) (void);
+	void (*indicator_service_manager_reserved2) (void);
+	void (*indicator_service_manager_reserved3) (void);
+	void (*indicator_service_manager_reserved4) (void);
 };
 
+/**
+	IndicatorServiceManager:
+	@parent: #GObject
+
+*/
 struct _IndicatorServiceManager {
 	GObject parent;
+
 };
 
 GType indicator_service_manager_get_type (void);
+
+IndicatorServiceManager *   indicator_service_manager_new         (gchar * dbus_name);
+gboolean                    indicator_service_manager_connected   (IndicatorServiceManager * sm);
+void                        indicator_service_manager_set_refresh (IndicatorServiceManager * sm,
+                                                                   guint time_in_ms);
 
 G_END_DECLS
 
