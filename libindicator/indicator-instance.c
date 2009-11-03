aaacaacde502d2593108db0a5bf4182b9552ce4d
Ted Gould <ted@canonical.com>
2009-11-03 11:52:16 -0600

    Adding in the basis for the instance class.

---
+++ libindicator/indicator-instance.c
@@ -0,0 +1,26 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "indicator-instance.h"
+
+static void indicator_instance_class_init (IndicatorInstanceClass *klass);
+static void indicator_instance_init       (IndicatorInstance *self);
+
+G_DEFINE_TYPE (IndicatorInstance, indicator_instance, G_TYPE_OBJECT);
+
+static void
+indicator_instance_class_init (IndicatorInstanceClass *klass)
+{
+
+	return;
+}
+
+static void
+indicator_instance_init (IndicatorInstance *self)
+{
+	/* No Data for this Class */
+
+	return;
+}
+
