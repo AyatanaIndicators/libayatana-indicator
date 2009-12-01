aa951a3eb41ba3e4c9fcf9732992aa6e28dc83a8
Ted Gould <ted@gould.cx>
2009-12-01 16:34:32 -0600

    Change the watch function to return both an API version and a user set version.

--- libindicator/indicator-service-manager.c
+++ libindicator/indicator-service-manager.c
@@ -232,7 +232,7 @@ get_property (GObject * object, guint prop_id, GValue * value, GParamSpec * pspe
 }
 
 static void
-watch_cb (DBusGProxy * proxy, gint service_version, GError * error, gpointer user_data)
+watch_cb (DBusGProxy * proxy, guint service_api_version, guint this_service_version, GError * error, gpointer user_data)
 {
 	IndicatorServiceManagerPrivate * priv = INDICATOR_SERVICE_MANAGER_GET_PRIVATE(user_data);
 
@@ -242,8 +242,8 @@ watch_cb (DBusGProxy * proxy, gint service_version, GError * error, gpointer use
 		return;
 	}
 
-	if (service_version != INDICATOR_SERVICE_VERSION) {
-		g_warning("Service is using a different version of the service interface.  Expecting %d and got %d.", INDICATOR_SERVICE_VERSION, service_version);
+	if (service_api_version != INDICATOR_SERVICE_VERSION) {
+		g_warning("Service is using a different version of the service interface.  Expecting %d and got %d.", INDICATOR_SERVICE_VERSION, service_api_version);
 		return;
 	}
 
