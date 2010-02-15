95f07c4e018e2a257a765ee7f71c777d555f548d
Ted Gould <ted@gould.cx>
2010-02-15 14:13:51 -0600

    Copyright headers

--- libindicator/indicator-desktop-shortcuts.c
+++ libindicator/indicator-desktop-shortcuts.c
@@ -1,3 +1,26 @@
+/*
+A small file to parse through the actions that are available
+in the desktop file and making those easily usable.
+
+Copyright 2010 Canonical Ltd.
+
+Authors:
+    Ted Gould <ted@canonical.com>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 3.0 as published by the Free Software Foundation.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License version 3.0 for more details.
+
+You should have received a copy of the GNU General Public
+License along with this library. If not, see
+<http://www.gnu.org/licenses/>.
+*/
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
