805edb88641378614175ad9f4f205bbdc1d707a1
Ted Gould <ted@gould.cx>
2010-03-10 13:10:36 -0600

    Pulled out a bunch of the code into a refresh function for signals.

--- libindicator/indicator-image-helper.c
+++ libindicator/indicator-image-helper.c
@@ -3,31 +3,28 @@
 
 const gchar * INDICATOR_NAMES_DATA = "indicator-names-data";
 
-GtkImage *
-indicator_image_helper (const gchar * name)
+void
+refresh_image (GtkImage * image)
 {
-	g_return_val_if_fail(name != NULL, NULL);
-	g_return_val_if_fail(name[0] != '\0', NULL);
+	g_return_if_fail(GTK_IS_IMAGE(image));
+
+	GIcon * icon_names = (GIcon *)g_object_get_data(G_OBJECT(image), INDICATOR_NAMES_DATA);
+	g_return_if_fail(icon_names != NULL);
 
 	/* Get the default theme */
 	GtkIconTheme * default_theme = gtk_icon_theme_get_default();
-	g_return_val_if_fail(default_theme != NULL, NULL);
-
-	/* Build us a GIcon */
-	GIcon * icon_names = g_themed_icon_new_with_default_fallbacks(name);
-	g_return_val_if_fail(icon_names != NULL, NULL);
+	g_return_if_fail(default_theme != NULL);
 
 	/* Look through the themes for that icon */
 	GtkIconInfo * icon_info = gtk_icon_theme_lookup_by_gicon(default_theme, icon_names, 22, 0);
 	if (icon_info == NULL) {
-		g_warning("Unable to find icon '%s' in theme.", name);
-		g_object_unref(icon_names);
-		return NULL;
+		g_warning("Unable to find icon in theme.");
+		return;
 	}
 
 	/* Grab the filename */
 	const gchar * icon_filename = gtk_icon_info_get_filename(icon_info);
-	g_return_val_if_fail(icon_filename != NULL, NULL); /* An error because we shouldn't get info without a filename */
+	g_return_if_fail(icon_filename != NULL); /* An error because we shouldn't get info without a filename */
 
 	/* Build a pixbuf */
 	GError * error = NULL;
@@ -35,14 +32,29 @@ indicator_image_helper (const gchar * name)
 	gtk_icon_info_free(icon_info);
 
 	if (pixbuf == NULL) {
-		g_error("Unable to load icon from name '%s' file '%s' because: %s", name, icon_filename, error == NULL ? "I don't know" : error->message);
-		g_object_unref(icon_names);
-		return NULL;
+		g_error("Unable to load icon from file '%s' because: %s", icon_filename, error == NULL ? "I don't know" : error->message);
+		return;
 	}
 
+	/* Put the pixbuf on the image */
+	gtk_image_set_from_pixbuf(image, pixbuf);
+	g_object_unref(G_OBJECT(pixbuf));
+
+	return;
+}
+
+GtkImage *
+indicator_image_helper (const gchar * name)
+{
+	g_return_val_if_fail(name != NULL, NULL);
+	g_return_val_if_fail(name[0] != '\0', NULL);
+
+	/* Build us a GIcon */
+	GIcon * icon_names = g_themed_icon_new_with_default_fallbacks(name);
+	g_return_val_if_fail(icon_names != NULL, NULL);
+
 	/* Build us an image */
-	GtkImage * image = GTK_IMAGE(gtk_image_new_from_pixbuf(pixbuf));
-	g_object_unref(pixbuf);
+	GtkImage * image = GTK_IMAGE(gtk_image_new());
 
 	if (image == NULL) {
 		g_error("Unable to create image from pixbuf on icon name '%s'", name);
@@ -52,6 +64,9 @@ indicator_image_helper (const gchar * name)
 
 	/* Attach our names to the image */
 	g_object_set_data_full(G_OBJECT(image), INDICATOR_NAMES_DATA, icon_names, g_object_unref);
+
+	/* Put the pixbuf in */
+	refresh_image(image);
 
 	/* Connect to all changes */
 	/* TODO */
