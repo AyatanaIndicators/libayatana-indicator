636b9f97d012198242832a74ac70a471098a66c8
Ted Gould <ted@gould.cx>
2010-03-10 11:45:53 -0600

    Adding in an image helper library.

---
+++ libindicator/indicator-image-helper.c
@@ -0,0 +1,12 @@
+
+#include "indicator-image-helper.h"
+
+GtkImage *
+indicator_image_helper (const gchar * name)
+{
+	g_return_val_if_fail(name != NULL, NULL);
+	g_return_val_if_fail(name[0] != '\0', NULL);
+
+
+	return NULL;
+}
