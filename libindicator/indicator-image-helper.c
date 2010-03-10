5867fa760a390d48f30e8794f6292601c61cefad
Ted Gould <ted@gould.cx>
2010-03-10 16:40:19 -0600

    Adding in a copyright header.

--- libindicator/indicator-image-helper.c
+++ libindicator/indicator-image-helper.c
@@ -1,3 +1,25 @@
+/*
+A little helper to make a themed image with fallbacks that
+is only constrained in the vertical dimention.
+
+Copyright 2010 Canonical Ltd.
+
+Authors:
+    Ted Gould <ted@canonical.com>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 3.0 as published by the Free Software Foundation.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License version 3.0 for more details.
+
+You should have received a copy of the GNU General Public
+License along with this library. If not, see
+<http://www.gnu.org/licenses/>.
+*/
 
 #include <math.h>
 #include "indicator-image-helper.h"
