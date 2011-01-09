bea872dbb0a63719d253d04c4236b707b12fee80
Ted Gould <ted@gould.cx>
2011-01-09 14:31:00 -0600

    Marshaller fixes

--- libindicator/indicator-object-marshal.h
+++ libindicator/indicator-object-marshal.h
@@ -30,6 +30,14 @@ extern void _indicator_object_marshal_VOID__POINTER_UINT (GClosure     *closure,
                                                           gpointer      invocation_hint,
                                                           gpointer      marshal_data);
 
+/* VOID:POINTER,BOOLEAN (./indicator-object-marshal.list:4) */
+extern void _indicator_object_marshal_VOID__POINTER_BOOLEAN (GClosure     *closure,
+                                                             GValue       *return_value,
+                                                             guint         n_param_values,
+                                                             const GValue *param_values,
+                                                             gpointer      invocation_hint,
+                                                             gpointer      marshal_data);
+
 G_END_DECLS
 
 #endif /* ___indicator_object_marshal_MARSHAL_H__ */
